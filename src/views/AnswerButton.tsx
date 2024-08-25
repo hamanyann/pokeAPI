8ba93a99f20aa1e61290f1acdbdb74ecc39c44eb
hama <75150231+hamanyann@users.noreply.github.com>
2024-08-25 10:56:51 +0900

    perfect

--- src/views/AnswerButton.tsx
+++
@@ -1,8 +0,0 @@
-const AnswerButton = () => {
-    return (
-        <div>
-            <h1 className="text-3xl">Answer</h1>
-        </div>
-    )
-}
-export default AnswerButton
--- src/views/Question.tsx
+++
@@ -1,8 +0,0 @@
-const Question = () => {
-    return (
-        <div>
-            <h1 className="text-3xl">Question</h1>
-        </div>
-    )
-}
-export default Question
